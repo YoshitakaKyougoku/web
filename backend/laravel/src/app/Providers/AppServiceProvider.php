e1b096e9c9b5b5d2fbddf03c66aa0f6f8f8f4099
YoshitakaKyougoku <114034035+YoshitakaKyougoku@users.noreply.github.com>
2023-09-04 10:22:32 +0900

    commit

---
+++ backend/laravel/src/app/Providers/AppServiceProvider.php
@@ -0,0 +1,28 @@
+<?php declare(strict_types=1);
+
+namespace App\Providers;
+
+use Illuminate\Support\ServiceProvider;
+use Laravel\Sanctum\Sanctum;
+class AppServiceProvider extends ServiceProvider
+{
+    /**
+     * Register any application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        Sanctum::ignoreMigrations();
+    }
+
+    /**
+     * Bootstrap any application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+}
---
+++ backend/laravel/src/routes/api.php
@@ -0,0 +1,19 @@
+<?php declare(strict_types=1);
+
+use App\Http\Controllers\Api\MeController;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+Route::group(['middleware' => ['auth:sanctum']], function () {
+    Route::get('/me', MeController::class);
+});
